1b7ed98e8e2e29315085d01fdb7d7985237801c5
Slava <81441961+Hil-sys@users.noreply.github.com>
2021-12-29 21:12:26 +0300

    Add files via upload

---
+++ 15.2/задача 5/z5.py
@@ -0,0 +1,15 @@
+r = 0
+k = 0
+n = int(input())
+s = []
+for i in range(n):
+    a = int(input())
+    s.append(a)
+    if a <= 30:
+        k+=1
+s.sort()
+sm = min(s)
+sx = max(s)
+r = sx - sm
+print(r)
+print(k)
